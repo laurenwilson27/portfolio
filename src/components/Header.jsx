6205c4dd0e2cbbac43cf88d24aa302254ec5c359
laurenwilson27 <140430039+laurenwilson27@users.noreply.github.com>
2023-12-19 22:45:12 -0330

    Basic header, links are not yet functional

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header>
+      <div className="nametag">Lauren Wilson</div>
+      <nav>Projects &bull; Games &bull; Contact</nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Project.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Project = ({ index }) => {
+  return <div>Project {index}</div>;
+};
+
+export default Project;
