ed30697662e9f6045059b440fa87f67d6a3fc6dd
laurenwilson27 <140430039+laurenwilson27@users.noreply.github.com>
2023-12-21 22:25:26 -0330

    Stopped propogation on GitHub Links

--- src/components/GitHubLink.jsx
+++ src/components/GitHubLink.jsx
@@ -3,7 +3,12 @@ import { Link } from "react-router-dom";
 
 const GitHubLink = ({ repo }) => {
   return (
-    <Link to={repo} target="_blank" rel="noopener noreferrer">
+    <Link
+      to={repo}
+      target="_blank"
+      rel="noopener noreferrer"
+      onClick={(e) => e.stopPropagation()}
+    >
       <div className="tag gitHub">
         <span className="tagText">View on GitHub</span>
       </div>
